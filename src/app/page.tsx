20f93281049132b5b4df60198b3f602bffa7e2c4
khalidbelhadj <106029014+khalidbelhadj@users.noreply.github.com>
2024-03-02 23:11:17 +0000

    Search bar implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import SearchResult from "@/components/SearchResult";
 import { Button } from "@/components/ui/button";
+import { SearchIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        HELLO WORLD
-      </p>
-      <Button>Click me</Button>
+    <main className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-md border">
+      <div className="border flex rounded-full px-3 py-2 bg-white items-center gap-2 focus-within:border-[1.5px] focus-within:border-black w-80">
+        <SearchIcon className="w-5 h-5 text-gray-300" />
+        <input
+          name="search-box"
+          className="focus:outline-none w-full"
+          placeholder="Search"
+        />
+      </div>
     </main>
   );
 }
